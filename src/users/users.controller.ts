4462c8d1263da1fa0dfb202bb2da6e80c22d6f07
nch <nch@anadeainc.com>
2021-10-10 17:30:43 +0300

    Add SerializeInterceptor

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -7,27 +7,28 @@ import { Controller,
   Delete,
   Query,
   NotFoundException,
-  UseInterceptors,
-  ClassSerializerInterceptor
+  UseInterceptors
 } from '@nestjs/common'
-import { CreateUserDto } from './dtos/create-user.dto';
-import { UpdateUserDto } from './dtos/update-user.dto';
-import { UsersService } from './users.service';
-
+import { CreateUserDto } from './dtos/create-user.dto'
+import { UpdateUserDto } from './dtos/update-user.dto'
+import { UsersService } from './users.service'
+import { Serialize } from "../interceptors/serialize.interceptors"
+import { UserDto } from '../users/dtos/user.dto'
 
 @Controller('auth')
+@Serialize(UserDto)
 export class UsersController {
   constructor(private usersService: UsersService) {}
 
   @Post('/signup')
   createUser(@Body() body: CreateUserDto) {
-    this.usersService.create(body.email, body.password);
+    this.usersService.create(body.email, body.password)
   }
 
-  @UseInterceptors(ClassSerializerInterceptor)
+
   @Get('/:id')
   async findUser(@Param('id') id: string) {
-    const user = await this.usersService.findOne(parseInt(id));
+    const user = await this.usersService.findOne(parseInt(id))
     if(!user){
       throw new NotFoundException('User not found')
     }
@@ -36,16 +37,16 @@ export class UsersController {
 
   @Get()
   findAllUsers(@Query('email') email: string) {
-    return this.usersService.find(email);
+    return this.usersService.find(email)
   }
 
   @Delete('/:id')
   removeUser(@Param('id')  id: string) {
-    return this.usersService.remove(parseInt(id));
+    return this.usersService.remove(parseInt(id))
   }
 
   @Patch(':id')
   updateUser(@Param('id')  id: string, @Body() body: UpdateUserDto) {
-    return this.usersService.update(parseInt(id), body);
+    return this.usersService.update(parseInt(id), body)
   }
 }
