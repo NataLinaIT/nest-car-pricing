8fc180e44e4d29bc212169b03f78be4b82b2b033
nch <nch@anadeainc.com>
2021-10-16 16:29:09 +0300

    Signup | signin

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -12,17 +12,26 @@ import { Controller,
 import { CreateUserDto } from './dtos/create-user.dto'
 import { UpdateUserDto } from './dtos/update-user.dto'
 import { UsersService } from './users.service'
+import { AuthService } from './auth.service';
 import { Serialize } from "../interceptors/serialize.interceptors"
 import { UserDto } from '../users/dtos/user.dto'
 
 @Controller('auth')
 @Serialize(UserDto)
 export class UsersController {
-  constructor(private usersService: UsersService) {}
+  constructor(
+    private usersService: UsersService,
+    private authService: AuthService
+    ) {}
 
   @Post('/signup')
   createUser(@Body() body: CreateUserDto) {
-    this.usersService.create(body.email, body.password)
+    return this.authService.signUp(body.email, body.password)
+  }
+
+  @Post('/signin')
+  signIn(@Body() body: CreateUserDto) {
+    return this.authService.signIn(body.email, body.password)
   }
 
 
