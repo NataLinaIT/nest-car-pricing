d5d0613777745869a4ccd6e6c1172bbccd505549
nch <nch@anadeainc.com>
2021-10-30 19:27:35 +0300

    Add auth guard

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -8,6 +8,7 @@ import { Controller,
   Query,
   NotFoundException,
   Session,
+  UseGuards
 } from '@nestjs/common'
 import { CreateUserDto } from './dtos/create-user.dto'
 import { UpdateUserDto } from './dtos/update-user.dto'
@@ -17,6 +18,7 @@ import { Serialize } from "../interceptors/serialize.interceptors"
 import { UserDto } from '../users/dtos/user.dto'
 import { CurrentUser } from "./decorators/current-user.decorator";
 import { User } from "./user.entity";
+import { AuthGuard } from "../guards/auth.guard";
 
 @Controller('auth')
 @Serialize(UserDto)
@@ -32,6 +34,7 @@ export class UsersController {
   // }
 
   @Get('/whoami')
+  @UseGuards(AuthGuard)
   whoAmi(@CurrentUser() user: User) {
     return user
   }
